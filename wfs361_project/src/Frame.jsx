3aa630ad7a5a979577e496c5cc67b3b4d11c5676
Jedijr26 <132846193+Jedijr26@users.noreply.github.com>
2025-05-04 23:25:48 +0200

    Add files via upload

---
+++ wfs361_project/src/Frame.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FrameOne from './components/FrameOne';
+import './Index.css';
+
+function Frame() {
+  return (
+    <div>
+      <FrameOne/>
+    </div>
+  )
+}
+
+export default Frame
---
+++ wfs361_project/tailwind.config.js
@@ -0,0 +1,93 @@
+module.exports = {
+  content: [
+    "./src/**/*.jsx",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "bg-gray": "var(--bg-gray)",
+        "bg-line": "var(--bg-line)",
+        "bg-white": "var(--bg-white)",
+        "brand-yellow": "var(--brand-yellow)",
+        "font-high-emphasis": "var(--font-high-emphasis)",
+        "font-low-emphasis": "var(--font-low-emphasis)",
+        "font-medium-emphasis": "var(--font-medium-emphasis)",
+        "x-1": "var(--x-1)",
+        "x-2": "var(--x-2)",
+        "x-4": "var(--x-4)",
+        "x-5": "var(--x-5)",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      fontFamily: {
+        "body-18px": "var(--body-18px-font-family)",
+        "body-24px": "var(--body-24px-font-family)",
+        h1: "var(--h1-font-family)",
+        "title-h1": "var(--title-h1-font-family)",
+        "title-h2": "var(--title-h2-font-family)",
+        sans: [
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+    container: { center: true, padding: "2rem", screens: { "2xl": "1400px" } },
+  },
+  plugins: [],
+  darkMode: ["class"],
+};
